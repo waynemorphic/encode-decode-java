ccdce6acafd25bb5b151ee3212ff5a1de1f041a2
waynemorphic <wayneklyde@gmail.com>
2022-10-29 19:34:52 +0300

    Encrypting class with Encode method that encrypts sentence passed according to the shift key integer value

--- src/main/java/Models/Encode.java
+++ src/main/java/Models/Encode.java
@@ -1,17 +1,23 @@
 package Models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Encode {
     private String letters;
-    private String cypherLetters;
+    private int shiftKey;
+    public static final String Alphabets = "abcdefghijklmnopqrstuvwxyz";
 
     public static void main(String[] args){
+        Encode encode = new Encode("HI", 2);
+        System.out.println(encode.encrypt(encode.getLetters(), encode.getShiftKey()));
 
     }
 
     // constructor of the private class
-    public Encode(String letters, String cypherLetters){
+    public Encode(String letters, int shiftKey){
         this.letters = letters;
-        this.cypherLetters = cypherLetters;
+        this.shiftKey = shiftKey;
     }
 
     // Getter and setter methods
@@ -19,12 +25,12 @@ public class Encode {
         return letters;
     }
 
-    public String getCypherLetters() {
-        return cypherLetters;
+    public int getShiftKey() {
+        return shiftKey;
     }
 
-    public void setCypherLetters(String cypherLetters) {
-        this.cypherLetters = cypherLetters;
+    public void setShiftKey(int shiftKey) {
+        this.shiftKey = shiftKey;
     }
 
     public void setLetters(String letters) {
@@ -32,13 +38,26 @@ public class Encode {
     }
 
     // Method to encode characters
-    public String encoder(String letters, String cypherLetters){
-        char[] arrLetters = letters.toCharArray();
-        char[] alphs = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-        char[] cyphers = {'X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W'};
-        for (int i = 0; i < arrLetters.length; i++){
+    public String encrypt(String letters, int shiftKey){
+        letters = letters.toLowerCase();
+        String cipherText = "";
 
+        /*
+        * - for encryption, we are using the formula,
+        * --- En(X) = (X + n) mod 26
+        * - where:
+        *       - n is the shift key
+        *       - 26 is the number of alphabets
+        *       - X is the individual character position
+        *       - En is the encrypted character with the applied shift key
+        */
+
+        for (int i = 0; i < letters.length(); i++){
+            int charPosition = Alphabets.indexOf(letters.charAt(i));
+            int keyValue = (shiftKey + charPosition) % 26;
+            char replaceValue = Alphabets.charAt(keyValue);
+            cipherText += replaceValue;
         }
-        return cypherLetters;
+        return cipherText;
     }
 }
